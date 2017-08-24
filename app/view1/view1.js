959577e7fee3df4025f10481620623a35e2dd1d7
pdrumm <pdrumm@nd.edu>
2017-08-23 21:38:45 -0400

    Created a skeleton Connect4 Board using AngularJs and flexbox

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,53 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
+  $scope.emptySlot = ".";
+  $scope.boardWidth = 7;
+  $scope.boardHeight = 6;
+  function Board(width, height) {
+    const row = Array(width).fill($scope.emptySlot);
+    return Array(height).fill(row);
+  }
+  $scope.board = Board($scope.boardWidth, $scope.boardHeight);
+})
 
-}]);
+.directive('c4Board', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      board: '='
+    },
+    templateUrl: 'view1/c4Board.html'
+  }
+})
+
+.directive('c4Row', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      row: '='
+    },
+    templateUrl: 'view1/c4Row.html'
+  }
+})
+
+.directive('c4Grid', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      value: '@'
+    },
+    templateUrl: 'view1/c4Grid.html'
+  }
+})
+
+.directive('c4Disc', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      value: '@'
+    },
+    templateUrl: 'view1/c4Disc.html'
+  }
+});
