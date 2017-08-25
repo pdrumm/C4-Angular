cb69775bb2d3c5b8e65c2d7d1f3ba7e9615494a7
pdrumm <pdrumm@nd.edu>
2017-08-24 22:11:24 -0400

    Added ability to drop token into the board, along with simple animation

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,12 +10,17 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope) {
-  $scope.emptySlot = ".";
+  $scope.emptyGrid = '.';
+  $scope.player = '1';
+  $scope.opponent = '2';
   $scope.boardWidth = 7;
   $scope.boardHeight = 6;
   function Board(width, height) {
-    const row = Array(width).fill($scope.emptySlot);
-    return Array(height).fill(row);
+    let board = [];
+    for (let i=0; i<width; ++i) {
+      board.push(Array(height).fill($scope.emptyGrid));
+    }
+    return board;
   }
   $scope.board = Board($scope.boardWidth, $scope.boardHeight);
 })
@@ -30,13 +35,59 @@ angular.module('myApp.view1', ['ngRoute'])
   }
 })
 
-.directive('c4Row', function() {
+.directive('c4Col', function($timeout) {
+
+  function getNextAvailableGrid(col, emptyGrid) {
+    let nextGrid = -1;
+    for (let i=0; i<col.length; ++i) {
+      if (col[i] === emptyGrid) {
+        nextGrid = i;
+      }
+    }
+    return nextGrid;
+  }
+
+  function animateDrop(discElems, stopIdx) {
+    for (let i=0; i<=stopIdx; ++i) {
+      $timeout(function() {
+        angular.element(discElems[i]).addClass('disc-player');
+        angular.element(discElems[i-1]).removeClass('disc-player');
+      }, i * 400);
+    }
+  }
+
+  function link(scope, element, attrs) {
+    element.on('mouseenter', function() {
+      let nextGridIdx = getNextAvailableGrid(scope.col, scope.$parent.$parent.$parent.emptyGrid);
+      if (nextGridIdx < 0)
+        return;
+      let nextDisc = angular.element(element.find('c4-disc')[nextGridIdx]);
+      nextDisc.addClass('disc-hover');
+    });
+
+    element.on('mouseleave', function() {
+      element.find('c4-disc').removeClass('disc-hover');
+    });
+
+    element.on('click', function() {
+      let nextGridIdx = getNextAvailableGrid(scope.col, scope.$parent.$parent.$parent.emptyGrid);
+      if (nextGridIdx < 0)
+        return;
+      scope.col[nextGridIdx] = scope.$parent.$parent.$parent.player;
+      let nextDisc = angular.element(element.find('c4-disc')[nextGridIdx]);
+      nextDisc.removeClass('disc-hover');
+
+      animateDrop(angular.element(element.find('c4-disc')), nextGridIdx);
+    });
+  }
+
   return {
     restrict: 'E',
     scope: {
-      row: '='
+      col: '='
     },
-    templateUrl: 'view1/c4Row.html'
+    templateUrl: 'view1/c4Col.html',
+    link: link
   }
 })
 
